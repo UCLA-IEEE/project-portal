cd0019b40a63ae5b474ca8c1b9aa7721b70778fd
maggie huang <maggie.huang9@gmail.com>
2019-04-17 20:58:25 -0700

    add new file

---
+++ server/model/project.py
@@ -0,0 +1,57 @@
+from app import db
+from common.errors import ResourceExistsError, ResourceDoesNotExistError
+
+class Project(db.Model):
+    name = db.Column(db.String(20), unique=True, nullable=False)
+    description = db.Column(db.String(120), nullable=False)
+
+    def __repr__(self):
+        return '<Project %r>' % self.name
+    
+    def to_dict(self):
+        return {
+            'name': self.name,
+            'description': self.description
+        }
+
+# Add test data
+test_users = [
+    Project(name="OPS", description="circuits"),
+    Project(name="Micromouse", project="autonomous maze-solving robot")
+]
+
+def does_project_exist(name):
+    return Project.query.filter_by(name=name).count() != 0
+
+for project in test_projects:
+    if not does_project_exist(project.name):
+        db.session.add(project)
+        db.session.commit()
+
+def create_project(name, description):
+    if does_project_exist(name):
+        raise ResourceExistsError
+
+    new_project = Project(name=name, description=description)
+    db.session.add(new_project)
+    db.session.commit()
+    return new_project
+
+def get_all_projects():
+    return Project.query.all()
+
+def get_project(name):
+    if not does_project_exist(name):
+        raise ResourceDoesNotExistError
+
+    return Project.query.filter_by(name=name).first()
+
+def delete_project(name):
+    if not does_project_exist(name):
+        raise ResourceDoesNotExistError
+    
+    project = Project.query.filter_by(name=name).first()
+    db.session.delete(project)
+    db.session.commit()
+
+return project
