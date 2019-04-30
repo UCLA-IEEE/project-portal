a7708cb308b3c3cdb927b594df1da74eeaabae1c
Robert Zalog <rzalog@gmail.com>
2019-04-29 17:08:09 -0700

    Make User model correct. Also change some DB stuff.

--- server/db-restart.py
+++ server/db-restart.py
@@ -1,4 +1,5 @@
 from app import db
+from load_mock_data import load
 
 print("WARNING: This will drop all data in the DB. Are you sure you want to do this?")
 resp = input("(Y/n): ")
@@ -6,3 +7,4 @@ resp = input("(Y/n): ")
 if resp.lower() == "yes" or resp.lower() == "y":
     db.drop_all()
     db.create_all()
+    load()
