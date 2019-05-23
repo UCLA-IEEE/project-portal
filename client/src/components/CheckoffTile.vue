b07d49fd77827185b1e6929af1a66c2c604c70ed
Eugene Lo <bleugene12@g.ucla.edu>
2019-05-23 00:54:39 -0700

    added dropdown menu

--- client/src/components/CheckoffTile.vue
+++ client/src/components/CheckoffTile.vue
@@ -6,26 +6,64 @@
         <div id="progress-block">
             <p>{{ project.progress }}%</p>
         </div>
-        <div class="dropdown">
-
-        </div>
+        <DropdownButton v-on:open='setOpen'></DropdownButton>
+        <transition name='expand'>
+            <div v-if="open" class="dropdown-backdrop">
+            </div>
+        </transition>
     </div>       
 </template>
 
 <script>
+import DropdownButton from './DropdownButton'
 export default {
     name: 'CheckoffTile',
+    data() {
+        return {
+            open: false 
+        } 
+    },
+    components: {
+        DropdownButton
+    },
     props: {
         project: Object
+    },
+    methods: {
+        setOpen() {
+            this.open = !this.open;
+        }
     }
 }
+
+
 </script>
 
 <style>
 .project-progress {
-    margin: 0px 40px;
+    display: grid;
+    grid-template-columns: 60% 30% 10%;
+    grid-template-rows: auto auto;
+    position: relative;
+    margin: auto 40px;
     text-align: left;
     border-top: 1px solid #1F6891;
+}
+
+.show {
+    display: block;
+}
+
+.dropdown-backdrop {
+    background-color: #EFEFEF;
+    border-top: 5px solid;
+    min-height: 200px;
+    position: relative;
+    width: 100%;
+    grid-column-start: 1;
+    grid-column-end: 4;
+    grid-row-start: 2;
+    grid-row-end: 3;
 }
 
 #checkoff-title {  
@@ -35,22 +73,47 @@ export default {
 }
 
 #title-block {
+    margin-top: 10px;
     font-weight: bold;
     margin-left: 40px;
-    width: 60%;
-    display: inline-block;
-    height: 10px;
+    grid-column-start: 1;
+    grid-column-end: 2;
+    grid-row-start: 1;
+    grid-row-end: 2;
+    position: relative;
 }
 
 #progress-block {
-    width: 30%;
-    display: inline-block;
-    height: 10px;
+    margin-top: 10px;
+    grid-column-start: 2;
+    grid-column-end: 3;
+    grid-row-start: 1;
+    grid-row-end: 2;
+    position: relative;
 }
 
-.dropdown {
-    background-color: #EFEFEF;
-    border-top: 5px solid 
+button:focus {
+    outline: 0;
 }
+
+button:active{
+    background-color: #eee;
+    color: #eee;
+}
+
+.expand-enter-active, .expand-leave-active {
+    transition: all .3s ease-in-out;
+    height: 30px;
+    padding: 10px;
+    background-color: #eee;
+    overflow: hidden;
+}
+
+.expand-enter, .expand-leave {
+    height: 0;
+    padding: 0 10px;
+    opacity: 0;
+}
+
 </style>
 
---
+++ client/src/components/DropdownButton.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="dropdown">
+        <button href='#' ref="button" @click="$emit('open') " class="glyphicon glyphicon-chevron-down" id="arrow"
+            type="button">
+        </button>
+    </div>
+    
+</template>
+
+<script>
+export default {
+    name: "DropdownButton",
+    props: {
+        btnClass: { type: String, default: 'btn-default' },
+        text: String
+    }
+}
+</script>
+
+<style>
+#arrow {
+    margin-top: 10px;
+    grid-column-start: 3;
+    grid-column-end: 4;
+    background: transparent;
+    border: none !important;
+}
+</style>
