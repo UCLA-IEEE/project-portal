db40e30390d6a7ea36672b80c322ddb5fced9cf3
robertnperalta <47515219+robertnperalta@users.noreply.github.com>
2019-06-01 16:51:55 -0700

    Base mobile navbar

--- client/src/components/MobileNavigation.vue
+++ client/src/components/MobileNavigation.vue
@@ -1,11 +1,23 @@
 <template>
   <div class="mobile-navbar">
-    <img class="mobile-logo" src="../../public/ieee-logo.png"/>
-    <svg class="mobile-menu" @click="clickMenu" :style="{ fill: menuColor }">
-      <rect class="rectangle" y="0"/>
-      <rect class="rectangle" y="8"/>
-      <rect class="rectangle" y="16"/>
-    </svg>
+    <div class="mobile-navbar-main">
+      <img class="mobile-logo" src="../../public/mobile-ieee-logo.png"/>
+      <svg class="mobile-icon" @click="clickMenu" :style="{ fill: menuColor }">
+        <rect class="rectangle" y="0"/>
+        <rect class="rectangle" y="8"/>
+        <rect class="rectangle" y="16"/>
+      </svg>
+    </div>
+    <div class="mobile-dropdown" v-show="displayMenu">
+      <router-link class="mobile-links" v-for="routes in links"
+      :key="routes.id" :to="`${routes.page}`">{{routes.text}}</router-link>
+      <button class='mobile-sign-button' v-if="!this.$store.state.authenticated" v-on:click="redirectLogin()">
+        <router-link :to="`/Login`">Sign In</router-link>
+      </button>
+      <button class='mobile-sign-button' v-else v-on:click="handleLogout()">
+        <router-link :to="`/Login`">Sign Out</router-link>
+      </button>
+    </div>
   </div>
 </template>
 
@@ -17,23 +29,23 @@ export default {
       links: [
         {
           id: 0,
-          text: 'Home',
-          page:'/'
-        },
-        {
-          id: 1,
           text: 'OPS',
           page:'/OPS'
         },
         {
-          id: 2,
-          text: 'Micromouse',
+          id: 1,
+          text: 'MM',
           page:'/Micromouse'
         },
         {
-          id: 3,
-          text: 'Aircopter',
+          id: 2,
+          text: 'AP',
           page:'/Aircopter'
+        },
+        {
+          id: 4,
+          text: 'Secure',
+          page: '/secure'
         }
       ],
       displayMenu: false
@@ -45,6 +57,15 @@ export default {
         this.displayMenu = true
       else
         this.displayMenu = false
+    },
+    redirectLogin() {
+      this.$router.replace({ name: "Login" });
+    },
+    handleLogout() {
+      this.$store.commit('updateAuthenticated', false);
+      if(!this.$store.state.authenticated) {
+        this.$router.replace({ name: "Login" });
+      }
     }
   },
   computed: {
@@ -58,17 +79,20 @@ export default {
 </script>
 
 <style>
-  .mobile-navbar {
+  .mobile-navbar-main {
     display: flex;
     height: 45px;
-    margin: 10px 15px;
     padding: 10px 20px;
     align-items: center;
     justify-content: space-between;
+
+    background-color: white;
+    box-shadow: 0px 5px 5px rgba(0, 0, 0, 0.2);
   }
-  .mobile-menu {
+  .mobile-icon {
     height: 18px;
     width: 24px;
+    transition: fill .5s;
   }
   .rectangle {
     height: 2px;
@@ -76,5 +100,33 @@ export default {
   }
   .mobile-logo {
     width: 45px;
+  }
+
+  .mobile-dropdown {
+    display: flex;
+    height: 40px;
+    width: inherit;
+    padding: 10px 40px;
+    align-items: center;
+    justify-content: space-between;
+
+    background: rgba(31, 104, 145, 0.85);
+    box-shadow: 0px 5px 5px rgba(0, 0, 0, 0.2);
+  }
+  .mobile-dropdown a, .mobile-sign-button a {
+    color: white;
+  }
+  .mobile-dropdown a:hover {
+    border-bottom: 1px solid white;
+  }
+  .mobile-sign-button a:hover {
+    border-bottom: none;
+  }
+  .mobile-sign-button {
+    background-color: inherit;
+    padding: 8px 20px;
+    border: 1px solid white;
+    border-radius: 5px;
+    margin-left: 30px;
   }
 </style>
