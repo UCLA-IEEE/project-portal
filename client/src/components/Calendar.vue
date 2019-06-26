58458674ba263f50dc8b3d84b97c79cd85d31090
robertnperalta <47515219+robertnperalta@users.noreply.github.com>
2019-06-26 12:05:52 -0700

    Basic import and parsing

--- client/src/components/Calendar.vue
+++ client/src/components/Calendar.vue
@@ -11,34 +11,53 @@ export default {
   components: {
     CalendarEvent
   },
+  created: function () {
+    this.importEvents()
+  },
+  methods: {
+    importEvents: function () {
+
+      var self = this
+      var globalId = 0  // Id element for v-bind:key
+      var refined = []
+      var nPagesLoaded = 0
+
+      var i
+      for (i = 1; i <= self.N_PAGES; i++) {   // Loads data for every page in the spreadsheet
+        var xhr = new XMLHttpRequest()
+
+        xhr.onload = function () {
+          var raw = JSON.parse(this.responseText)
+          raw = raw.feed.entry
+          delete raw[0]    // Remove formatting row
+          var j
+          for (j in raw) { // Extract necessary fields
+            var temp = { 'id': null, 'date': null, 'title': null, 'location': null }
+            temp.id = globalId++
+            temp.date = self.parseDate(raw[j].gsx$datemmddyyyy.$t)
+            temp.title = raw[j].gsx$name.$t
+            temp.location = raw[j].gsx$location.$t
+            refined.push(temp)
+          }
+          nPagesLoaded++
+
+          if (nPagesLoaded == self.N_PAGES) {  // After loading last page, store event data
+            self.calEvents = refined
+          }
+        }
+
+        xhr.open('GET', 'https://spreadsheets.google.com/feeds/list/19eixWfc2iZl5JbjN2JqfQxr1KIkt-bDJm7NZrAg6NBM/' + i + '/public/values?alt=json')
+        xhr.send()
+      }
+    },
+    parseDate: function (raw) {
+      return raw
+    }
+  },
   data() {
     return {
-      calEvents: [
-        {
-          id: 1,
-          date: "nov 18",
-          title: "Project 7 Deadline",
-        },
-        {
-          id: 2,
-          date: "nov 30",
-          title: "Project 8 Lecture",
-          location: "ENGR IV 6302",
-          link: "http://ieeebruins.org/",
-        },
-        {
-          id: 3,
-          date: "dec 15",
-          title: "IEEE Barbecue Social",
-          location: "Sunset Rec Lawn",
-          link: "https://www.facebook.com/groups/uclaieee/",
-        },
-        {
-          id: 4,
-          date: "dec 20",
-          title: "Winter Break Starts",
-        },
-      ]
+      calEvents: [],
+      N_PAGES: 6
     }
   }
 }
