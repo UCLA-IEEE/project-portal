dc0082feb8a5b0e204c771c043aafb17b1184b63
robertnperalta <47515219+robertnperalta@users.noreply.github.com>
2019-08-05 11:41:06 -0700

    Fix for sidebar calendar

--- client/src/components/CalendarEvent.vue
+++ client/src/components/CalendarEvent.vue
@@ -1,13 +1,13 @@
 <template>
-  <a class="calendar-event" :href="calEvent.link">
+  <div class="calendar-event">
     <div class="date-background">
       <p class="event-date">{{calEvent.date}}</p>
     </div>
-    <div class="event-info">
+    <a :class="[base, hasLink == '' ? noLink : '']" :href="calEvent.link">
       <p class="event-title">{{calEvent.title}}</p>
       <p class="event-location">{{calEvent.location}}</p>
-    </div>
-  </a>
+    </a>
+  </div>
 </template>
 
 <script>
@@ -15,6 +15,19 @@ export default {
   name: "CalendarEvent",
   props: {
     calEvent: Object
+  },
+  data() {
+    return {
+      base: "event-info",
+      noLink: "no-link",
+    }
+  },
+  computed: {
+    hasLink: function() {
+      if (typeof this.calEvent.link == 'undefined')
+        return false
+      return true
+    }
   }
 }
 </script>
@@ -31,6 +44,12 @@ export default {
   grid-column-gap: 20px;
   grid-template-areas: 'date info';
 }
+
+.no-link:hover {
+  color: black;
+  text-decoration: none;
+}
+
 .date-background {
   grid-area: date;
 
@@ -40,27 +59,32 @@ export default {
   background: #1F6891;
   border-radius: 5px;
 }
+
 .event-date {
   position: relative;
-  bottom: 6px;
+  top: 7.5px;
   left: 7.5px;
 
   height: 40px;
   width: 40px;
 
   text-transform: uppercase;
+  font-weight: 500;
   text-align: center;
   color: white;
 }
+
 .event-info {
   grid-area: info;
   margin: 10px 0px;
 
   font-size: 13px;
 }
+
 .event-title {
   margin: 0px;
 }
+
 .event-location {
   margin: 0px;
   font-size: 11px;
