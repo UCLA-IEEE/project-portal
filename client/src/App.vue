7e402dddfe8f5e6e1fb08c6c1bbecb2435133ca0
robertnperalta <47515219+robertnperalta@users.noreply.github.com>
2020-09-09 12:12:12 -0700

    Include jQuery, Bootstrap JS, and Google Fonts link

--- client/src/App.vue
+++ client/src/App.vue
@@ -59,7 +59,7 @@ export default {
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Josefin Sans', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
