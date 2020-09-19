3cbe89bb8df678bb615cb2961e68a0bc5400ba1b
robertnperalta <47515219+robertnperalta@users.noreply.github.com>
2020-09-18 18:38:38 -0700

    Add footer to login page

--- client/src/components/Login.vue
+++ client/src/components/Login.vue
@@ -17,12 +17,18 @@
         <router-link :to="{name: 'Register'}">Create an account</router-link> | <router-link :to="{name: 'Reset'}">Forgot my password</router-link>
       </p>
     </div>
+
+    <Footer/>
   </div>
 </template>
 
 <script>
+  import Footer from "./Footer"
   export default {
     name: 'Login',
+    components: {
+      Footer
+    },
     data() {
       return {
         username: '',
@@ -52,6 +58,7 @@
 
 <style>
   #login-wrapper {
+    position: relative;
     background-color: var(--blue);
     padding-top: calc(50vh - 250px);
     min-height: calc(100vh - 68px);
